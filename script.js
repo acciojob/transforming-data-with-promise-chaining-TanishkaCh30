6f50a448599a38dd77d09299ad22c40f7d9c3d80
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-03-12 10:59:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-//your JS code here. If required.
+let btn = document.getElementById("btn");
+let inputValue = document.getElementById("ip");
+let op = document.getElementById("output");
+
+btn.addEventListener("click",()=>{
+	let value = inputValue.value;
+	new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+		op.innerText = `Result: ${value}`;
+		resolve(value);
+	},2000)
+	})
+	.then((value)=>{
+		return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			value = value*2;
+			op.innerText = `Result: ${value}`;
+			 resolve(value);
+		},2000)
+		})
+			})
+		.then((value)=>{
+			return new Promise((resolve,reject)=>{
+				setTimeout(()=>{
+					value = value-3;
+					op.innerText = `Result: ${value}`;
+					resolve(value);
+				},1000)
+			})
+		})
+	.then((value)=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				value = value/2;
+				op.innerText = `Result: ${value}`;
+				resolve(value);
+			},1000)
+		})
+	})
+	.then((value)=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				value = value+10;
+				op.innerText = `Final Result: ${value}`;
+				// resolve(value);
+			},1000);
+		})
+	})
+})
